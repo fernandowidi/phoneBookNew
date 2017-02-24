46d9588bfc017d65a6895e5adea5b0dbf945fa34
Fernando Widi Baskoro <fernando.baskoro@dwp.co.id>
2017-02-24 09:22:40 +0700

    Add Validation To Active Record

--- app/models/data_kontak.rb
+++ app/models/data_kontak.rb
@@ -1,3 +1,6 @@
 class DataKontak < ApplicationRecord
-  belongs_to :user 
+  belongs_to :user
+  validates_associated :user
+  validates :nama, :alamat, :no_telp, :almt_email, presence: true
+  validates :no_telp, numericality: { message: "Masukkan Angka" }
 end
