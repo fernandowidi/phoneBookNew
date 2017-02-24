affaf206d0b406e89cd3f9d69825e37b350c71b5
Fernando Widi Baskoro <fernando.baskoro@dwp.co.id>
2017-02-24 17:07:47 +0700

    Add rspec

---
+++ spec/controllers/data_kontak_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DataKontakController, type: :controller do
+
+end
---
+++ spec/models/data_kontak_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe DataKontak, type: :model do
+  ## pending "add some examples to (or delete) #{__FILE__}"
+  it "Simpan Data"
+end
