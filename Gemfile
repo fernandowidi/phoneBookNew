e9f373408a770664c02edfb88eb44d82d3742896
Fernando Widi Baskoro <fernando.baskoro@dwp.co.id>
2017-03-04 09:43:21 +0700

    rspec controller done

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'ffaker'
+  gem 'capybara'
 end
 
 group :development do
--- spec/controllers/data_kontak_controller_spec.rb
+++ spec/controllers/data_kontak_controller_spec.rb
@@ -15,11 +15,26 @@ RSpec.describe DataKontakController, type: :controller do
     get :index
   end
 
+  it "GET new" do
+    get :new, xhr: true
+  end
+
+  it "GET edit" do
+    @data = build(:data)
+    @data.save
+    get :edit, params: { id: @data.id }, xhr: true
+  end
+
   describe "POST create" do
     context "valid attribute" do
       it "membuat data phonebook" do
         kontak_params = FactoryGirl.attributes_for(:data)
         expect { post :create, params: { kontak: kontak_params } }.to change(DataKontak, :count).by(1)
+      end
+
+      it "redirect ke index home" do
+        post :create, params: { kontak: FactoryGirl.attributes_for(:data) }
+        expect {response}.should redirect_to data_kontak_index_path
       end
     end
     context "invalid attribute" do
@@ -27,19 +42,101 @@ RSpec.describe DataKontakController, type: :controller do
         kontak_params = FactoryGirl.attributes_for(:data, nama: nil)
         expect { post :create, params: { kontak: kontak_params } }.to change(DataKontak, :count).by(0)
       end
+
       it "field no telepon kosong" do
         kontak_params = FactoryGirl.attributes_for(:data, no_telp: nil)
         expect { post :create, params: { kontak: kontak_params } }.to change(DataKontak, :count).by(0)
       end
+
       it "field no telepon selain angka" do
         kontak_params = FactoryGirl.attributes_for(:data, no_telp: 'ahsgd')
         expect { post :create, params: { kontak: kontak_params } }.to change(DataKontak, :count).by(0)
       end
-      context "render new saat data invalid" do
-        kontak_params = FactoryGirl.attributes_for(:data, no_telp: nil)
-        subject { post :create, params: { kontak: kontak_params } }
-        it { should render_template (:new) }
+    end
+
+    context "render new saat data invalid" do
+      kontak_params = FactoryGirl.attributes_for(:data, no_telp: nil)
+      subject { post :create, params: { kontak: kontak_params } }
+      it { should render_template ('new') }
+    end
+  end
+
+  describe "PUT update" do
+    before (:each) do
+      @data = build(:data)
+      @data.save
+    end
+
+    context "valid attribute" do
+      it "mencari data yang diminta" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data) }
+        assigns(:kontak).should eq(@data)
       end
+
+      it "update nama" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, nama: "Ganet") }
+        @data.reload
+        @data.nama.should eq("Ganet")
+      end
+
+      it "update no telepon" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, no_telp: "008") }
+        @data.reload
+        @data.no_telp.should eq("008")
+      end
+
+      it "update alamat" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, alamat: "jojojojo") }
+        @data.reload
+        @data.alamat.should eq("jojojojo")
+      end
+
+      it "update email" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, almt_email: "blabla@yahoo.com") }
+        @data.reload
+        @data.almt_email.should eq("blabla@yahoo.com")
+      end
+
+      it "redirect ke index saat berhasil update" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data) }
+        expect {response}.should redirect_to data_kontak_index_path
+      end
+    end
+
+    context "invalid attribut" do
+      it "nama kosong" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, nama: nil) }
+        @data.reload
+        @data.nama.should_not eq(nil)
+      end
+
+      it "no telepon kosong" do
+        put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, no_telp: nil) }
+        @data.reload
+        @data.no_telp.should_not eq(nil)
+      end
+    end
+    context "render edit" do
+      subject { put :update, params: { id: @data.id, kontak: FactoryGirl.attributes_for(:data, nama: nil) } }
+      it { should render_template ('edit') }
+    end
+  end
+
+  describe "DELETE destroy" do
+    before (:each) do
+      @data = build(:data)
+      @data.save
+    end
+
+    it "hapus data kontak" do
+      expect{
+        delete :destroy, params: { id: @data.id }
+      }.to change(DataKontak, :count).by(-1)
+    end
+
+    it "redirect ke index" do
+      delete :destroy, params: { id: @data.id }
+      expect {response}.should redirect_to data_kontak_index_path
     end
   end
 end
